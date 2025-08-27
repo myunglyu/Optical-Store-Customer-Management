4b13f9b600e6c230822758b2b8056ba6dab20276
Myung Lyu <180991957+myunglyu@users.noreply.github.com>
2025-08-27 12:53:19 -0700

    Update the build script and .gitignore for macOS build output

--- PublishScript_macOS.sh
+++ PublishScript_macOS.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+rm -rf ./backend
+rm -rf ./WooriOptical.Electron/out
+rm -rf ./WooriOptical.Electron/backend
+
 dotnet publish WooriOptical/WooriOptical.csproj -c Release --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -o ./backend
 echo "Backend published to ./backend"
 cp ./WooriOptical/WooriOptical.db ./backend
-cp -R ./backend ../WooriOptical.Electron/backend
-echo "Backend copied to ../WooriOptical.Electron/backend"
+cp -R ./backend ./WooriOptical.Electron/backend
+echo "Backend copied to ./WooriOptical.Electron/backend"
 
 cd WooriOptical.Electron
 
 npm install
 echo "Node modules installed"
 npm run make
-echo "app packaged at /WooriOptical.Electron/out"
+cp -R ./out ../WooriOptical_Build_MacOS
+echo "Electron app built at WooriOptical_Build_MacOS"
+
+cd ..
+rm -rf ./backend
+rm -rf ./WooriOptical.Electron/out
+rm -rf ./WooriOptical.Electron/backend
+echo "Cleanup complete"
